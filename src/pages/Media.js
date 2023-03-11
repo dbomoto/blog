03f63e6840c863d1023c42e7cc17a52dbe03bfe6
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2023-03-11 17:33:07 +0800

    Update 03112023

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -1,12 +1,11 @@
 function Media() {
     return (
         <div className="fixed flex flex-col space-y-2 top-0 right-0">
-            <button type="button" className="vsm:hidden">MOBILE</button>
-            <button type="button" className="hidden sm:block md:hidden">VSM</button>
-            <button type="button" className="hidden md:block lg:hidden">SM</button>
-            <button type="button" className="lg:hidden">MD</button>
-            <button type="button" className="xl:hidden">LG</button>
-            <button type="button" className="2xl:hidden">XL</button>
+            <button type="button" className="hidden vsm:block sm:hidden">VSM</button>
+            <button type="button" className="hidden sm:block md:hidden">SM</button>
+            <button type="button" className="hidden md:block lg:hidden">MD</button>
+            <button type="button" className="hidden lg:block xl:hidden">LG</button>
+            <button type="button" className="hidden xl:block 2xl:hidden">XL</button>
             <button type="button" className="hidden 2xl:block">2XL</button>
         </div>
     )
